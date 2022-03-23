6e9c3b6213b1020531a457ee4a0602bb29e26409
Hazael Vite SPI <hazaelvite@quimica.unam.mx>
2022-03-23 10:56:50 -0600

    componentes para el header y footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-footer
+    padless
+    class="mt-auto"
+  >
+    <v-col
+      class="text-center"
+      cols="12"
+    >  
+      <v-card-text class="body-2">
+        <strong>Todos los derechos reservados {{ new Date().getFullYear() }} — Facultad de Química, UNAM.</strong>
+        Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. 
+        Contiene enlaces con diversos portales de entidades y organizaciones académicas, estudiantiles y profesionales, así como con páginas personales cuyos contenidos son de la responsabilidad
+        exclusiva de sus titulares. <a href="https://quimica.unam.mx/aviso-de-privacidad/" >Aviso de privacidad</a>
+      </v-card-text>
+    </v-col>
+  </v-footer>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+    }),
+  }
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="stylesHeader">
+        <v-container>
+            <v-row align="center" justify="center">
+                <v-col cols="3">
+                    <a href="https://quimica.unam.mx/" target="_blank">
+                        <v-img src="@/assets/logo_fq.png" height="90" contain></v-img>
+                    </a>
+                </v-col>
+                <v-col cols="5" align="center">
+                    <div id="titleHeader">
+                        Universidad Nacional Autónoma de México<br>
+                        Facultad de Química<br>
+                        Olimpiada de Química CDMX
+                    </div>
+                </v-col>
+                <v-col cols="3">
+                    <a href="http://www.unam.mx" target="_blank">
+                        <v-img src="@/assets/logo_unam.png" height="100" contain></v-img>
+                    </a>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style>
+    .stylesHeader{
+        background-color: #1c3d6c;
+        color: #fff;
+        font-weight: 500;
+        font-size: 24px;
+    }
+    @media (max-width: 839px){
+        #titleHeader{
+            display: none;
+        }
+    }
+</style>
