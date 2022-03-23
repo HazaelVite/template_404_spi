dc0792f1544134d120beb27eaac544942a0fca3a
Hazael Vite SPI <hazaelvite@quimica.unam.mx>
2022-03-23 10:54:39 -0600

    plantilla de error 404

---
+++ src/components/Error404.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('@/assets/logo-fq-gris.png')"
+          class="my-3"
+          contain
+          height="150"
+        />
+      </v-col>
+      <v-col cols="12">
+        <h2 class="v-heading text-h2 text-sm-h2 mb-4" color="grey lighten-2">
+          404
+        </h2>
+        <h4 class="text-h4 text-sm-h4 mb-4" color="grey lighten-2">
+          Esta página no está disponible
+        </h4>
+        <p class="text-subtitle-1 text-sm-subtitle-1" >
+          La página a la que estás tratando de llegar nunca existió en esta realidad, o ha migrado a un universo paralelo. 
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style>
+</style>
